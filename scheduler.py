5d1c45e7f6779e54bbb8a3338ca57702516a60db
helfer <helfer@mit.edu>
2015-12-12 21:45:27 -0500

    merge changes from master

--- scheduler.py
+++ scheduler.py
@@ -27,39 +27,50 @@ class FlexibleStreamingScheduler():
         # START DIRECTORY WATCHER
         self.dw.start()
 
+        #queryparser.init()
+
+        # INITIALIZE
+        timers = []
+
+        def parse_input(i):
+            return json.loads(i) if len(i) > 0 else {}
+
         # RUN LOOP
-        while True:
-            if self.inputs:
-                filename = os.path.abspath(
-                    os.path.join(self.watch_dir, self.inputs.popleft()))
-                print("Detected new file: %s" % filename)
-                if self.test_queries:
-                    queries = self.test_queries
-                else:
-                    queries = queryparser.get_active_queries()
-                print 'queries:', queries
+        for i in range(0,3):
+            queries = queryparser.get_active_queries()
+            print 'queries:', queries
 
-                start = time.time()
-                lines = wrapper.AggregateWrapper(self.sc.textFile(filename))
-                #     no minimum line param in case of empty file
-                total = lines.count()
+            lines = wrapper.AggregateWrapper(self.sc.textFile(filename))
+            #     no minimum line param in case of empty file
+            total = lines.count()
+            # START TIMER
+            start = time.time()
 
-                # Loads all URLs from input file and initialize their neighbors.
-                results = [q.apply(lines) for q in queries]
+            # reading the entire watch-dir
+            lines = wrapper.CommonSubqueryWrapper(self.sc.textFile(os.path.join(self.watch_dir, self.inputs[0] )))
+            #     no minimum line param in case of empty file
+            total = lines.count()
 
-                total = total.__eval__()
-                counts = [rdd.__eval__() for rdd in results]
-                end = time.time()
+            results = [q.apply(lines) for q in queries]
 
-                for i,c in enumerate(counts):
-                    print(">>> %s of %s tweets match the filter: %s." % (c, total, queries[i].where))
+            total = total.__eval__()
+            counts = [rdd.__eval__() for rdd in results]
+            # Loads all URLs from input file and initialize their neighbors.
 
-                if self.test_queries:
-                    print("TIME: %.2f seconds" % (end - start))
-                    return
-                else:
-                    queryparser.write_results_to_mongodb( queries, counts )
+            # STOP TIMER
+            end = time.time()
+            timers.append(end - start)
+
+            for i,c in enumerate(counts):
+                print(">>> %s of %s tweets match the filter: %s." % (c, total, queries[i].where))
+
+
+            queryparser.write_results_to_mongodb( queries, counts )
             time.sleep(0.1)
+
+        print( '>>>>>>>' )
+        print( timers )
+        print( sum(timers), len(timers), sum(timers)/len(timers) )
 
     def stop(self):
         self.dw.stop()
